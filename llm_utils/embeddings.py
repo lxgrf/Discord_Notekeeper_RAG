e098c6ceeedcd845183e935be037c4cad5e999be
Alex Griffiths <github@lxgrf.co.uk>
2024-11-06 17:38:11 +0000

    Added get_answer()

--- llm_utils/embeddings.py
+++ llm_utils/embeddings.py
@@ -1,7 +1,8 @@
-from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings, ChatOllama
 from pathlib import Path
 from langchain.schema import Document
 from langchain_chroma import Chroma
+from langchain.chains import RetrievalQA
 import os
 import sys
 
@@ -10,7 +11,9 @@ from data_fetch import fetch_docs_from_Notion
 
 project_root = Path(__file__).parents[1]
 persist_directory_root = project_root / "chroma_db"
+
 embedding_model = OllamaEmbeddings(model="all-minilm:l6-v2")
+answer_model = ChatOllama(model="llama3.1")
 
 def get_embeddings(dbase: str, force_refresh=False):
     persist_directory = f"{persist_directory_root}/{dbase}"
@@ -30,6 +33,17 @@ def get_embeddings(dbase: str, force_refresh=False):
                          embedding_function=embedding_model)
         
     return vectors
+
+def get_answer(question:str,dbase:str):
+    vectors = get_embeddings(dbase=dbase)
+    retriever = vectors.as_retriever(search_type="mmr",
+                                     search_kwargs={"k":12,
+                                                    "alpha":0.7})
+    qa_chain = RetrievalQA.from_chain_type(llm=answer_model,
+                                           chain_type="stuff",
+                                           retriever=retriever)
+    result = qa_chain.invoke(question)
+    return result['result']
 
 if __name__ == "__main__":
     vectors = get_embeddings(dbase="8d5dc8537d04457fa92a543a83ac397b",force_refresh=True)
